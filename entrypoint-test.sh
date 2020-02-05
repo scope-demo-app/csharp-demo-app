34ea0a551d1c84e02c4b179925874128e2b93aef
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-02-05 17:26:04 +0100

    changes

--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-/root/.dotnet/tools/scope-run dotnet test -c Release
 cd ./test/csharp-demo-app.benchmark/
 dotnet run -c Release
+
 cd ../..
+/root/.dotnet/tools/scope-run dotnet test
