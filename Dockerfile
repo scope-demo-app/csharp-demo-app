cd878d61e2a42bdc853cf1e876b1df6f7df50cd3
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-02-12 15:00:02 +0100

    Dockerfile changes and more log info

--- Dockerfile
+++ Dockerfile
@@ -14,8 +14,9 @@ WORKDIR /app/out
 
 # Build runtime image
 FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
+#RUN apt-get -y update && apt-get -y install git && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/man/?? /usr/share/man/??_*
 WORKDIR /app
 COPY --from=build-env /app/out .
 COPY --from=build-env /root/.dotnet/tools /root/.dotnet/tools
-COPY ./.git/. ./.git/
+COPY ./.git ./.git
 ENTRYPOINT ["/root/.dotnet/tools/scope-run", "dotnet", "./csharp-demo-app.dll"]
--- src/csharp-demo-app/Program.cs
+++ src/csharp-demo-app/Program.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace csharp_demo_app
 {
@@ -14,6 +9,22 @@ namespace csharp_demo_app
         public static void Main(string[] args)
         {
             Console.WriteLine("Starting...");
+
+            var gitInfo = ScopeAgent.Coverage.Utils.GitInfo.GetCurrent();
+            Console.WriteLine();
+            Console.WriteLine("From Git Folder:");
+            Console.WriteLine("  Repository: {0}", gitInfo.Repository);
+            Console.WriteLine("  Branch: {0}", gitInfo.Branch);
+            Console.WriteLine("  Commit: {0}", gitInfo.Commit);
+            Console.WriteLine("  SourceRoot: {0}", gitInfo.SourceRoot);
+            Console.WriteLine();
+            Console.WriteLine("From Agent Settings:");
+            Console.WriteLine("  Repository: {0}", ScopeAgent.Agent.Settings.Repository);
+            Console.WriteLine("  Branch: {0}", ScopeAgent.Agent.Settings.Branch);
+            Console.WriteLine("  Commit: {0}", ScopeAgent.Agent.Settings.Commit);
+            Console.WriteLine("  SourceRoot: {0}", ScopeAgent.Agent.Settings.SourceRoot);
+            Console.WriteLine();
+            
             CreateHostBuilder(args).Build().Run();
         }
 
