f92f355afda7a062cea93b73ab07cd82582fde33
Daniel Redondo <tonyredondo@gmail.com>
2020-05-14 20:39:45 +0200

    container wait

--- src/csharp-demo-app/Startup.cs
+++ src/csharp-demo-app/Startup.cs
@@ -39,6 +39,7 @@ namespace csharp_demo_app
             });
             services.AddHealthChecks();
             Console.WriteLine("Applying migrations...");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
             using var ctx = new ImagesContext();
             try
             {
@@ -46,7 +47,7 @@ namespace csharp_demo_app
             }
             catch
             {
-                Thread.Sleep(TimeSpan.FromSeconds(20));
+                Thread.Sleep(TimeSpan.FromSeconds(30));
                 ctx.Database.Migrate();
             }
         }
